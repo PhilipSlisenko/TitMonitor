3e2fa071ea6bb6f0f0feed73e0060b7360837eb4
Philip Slisenko <philip.slisenko@pmi.com>
2021-10-11 18:00:13 +0200

    Add Detector and DBWriter classes

--- src/config.py
+++ src/config.py
@@ -5,8 +5,11 @@ from dataclasses import dataclass
 class Config:
     stream_url: str = 'rtsp://192.168.0.101:554/ch0_0.h264'
     root_dir_to_save_images: str = '/Users/pslisenk/Documents/projects/camera/images/'
-    db_connection_string: str = ''
-    model_path: str = ''
+    model_path: str = '/Users/pslisenk/Documents/projects/camera/models/best.pt'
+    dbhost: str = 'localhost'
+    dbname: str = 'camera'
+    dbuser: str = 'postgres'
+    dbpassword: str = 'postgres'
 
 
 config = Config()
--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@
 - save image to disc
 - put image info, detection info into db
 """
+import copy
 import os
 import queue
 import signal
@@ -13,14 +14,14 @@ from datetime import datetime
 
 import cv2
 import numpy as np
+import psycopg2
 import torch
 from loguru import logger
 
-from src.config import Config, config
+from src.config import Config
 
 
-# bufferless VideoCapture
-class MyVideoCapture:
+class BufferlessVideoCapture:
     def __init__(self, stream_url: str):
         self.cap = cv2.VideoCapture(stream_url)
         if not self.cap.isOpened():
@@ -63,13 +64,67 @@ class DiscWriter:
         t.start()
 
 
+class Detector:
+    def __init__(self, model_path: str):
+        self.model = torch.hub.load('ultralytics/yolov5', 'custom',
+                                    path=model_path)
+
+    def detect(self, img: np.ndarray):
+        """"
+        :param img: image array as how it's read by cv2 imread: HxWxC in BGR
+        """
+        detection = self.model(img[:, :, ::-1])
+        return detection
+
+    def rename_detection_filename(self, detection, filename: str):
+        """
+        Renames filename inside detection object so that .save() method saves detection result with desired filename.
+        """
+        detection = copy.deepcopy(detection)
+        detection.files[0] = filename
+        return detection
+
+
+class DBWriter:
+    def __init__(self, dbhost: str, dbname: str, dbuser: str, dbpassword: str):
+        self.dbhost = dbhost
+        self.dbname = dbname
+        self.dbuser = dbuser
+        self.dbpassword = dbpassword
+        self._open_connection()
+
+    def _open_connection(self):
+        self.conn = psycopg2.connect(host=self.dbhost,
+                                     database=self.dbname,
+                                     user=self.dbuser,
+                                     password=self.dbpassword)
+
+    def _reopen_conn_if_closed(self):
+        if self.conn.closed:
+            self._open_connection()
+
+    def insert_image_metadata(self, image_id: str, timestamp: datetime, filename: str):
+        self._reopen_conn_if_closed()
+        sql = """INSERT INTO images(image_id, timestamp, filename) VALUES(%s, %s, %s);"""
+        with self.conn:
+            with self.conn.cursor() as cur:
+                cur.execute(sql, (image_id, timestamp, filename))
+
+    def insert_detection(self, detection_id: str, coords: datetime, class_: str, conf: float, image_id: str):
+        self._reopen_conn_if_closed()
+        sql = """INSERT INTO detections(detection_id, coords, class, conf, image_id) VALUES(%s, %s, %s, %s, %s);"""
+        with self.conn:
+            with self.conn.cursor() as cur:
+                cur.execute(sql, (detection_id, coords, class_, conf, image_id))
+
+
 class Operator:
     def __init__(self, config: Config):
         logger.info('Initing operator')
-        self.cap = MyVideoCapture(config.stream_url)
+        self.cap = BufferlessVideoCapture(config.stream_url)
         self.disc_writer = DiscWriter(config.root_dir_to_save_images)
-        # self.db_writer = DBWriter(config.db_connection_string)
-        # self.detector = Detector(config.model_path)
+        self.db_writer = DBWriter(config.dbhost, config.dbname, config.dbuser, config.dbpassword)
+        self.detector = Detector(config.model_path)
 
         signal.signal(signal.SIGINT, self._signal_handler)
 
